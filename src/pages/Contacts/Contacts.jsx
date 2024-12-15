acf3272a5988e0a8147ab62c37186d7f61dd5aea
Roman <romka22830@gmail.com>
2024-12-15 13:21:54 +0200

    add useEffect

--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -1,11 +1,18 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchContacts } from "../../redux/contacts/operations";
 import ContactForm from "../../components/ContactForm/ContactForm";
 import SearchBox from "../../components/SearchBox/SearchBox";
 import ContactList from "../../components/ContactList/ContactList";
-import { useSelector } from "react-redux";
 
 const Contacts = () => {
-  const isLoading = useSelector((state) => state.contacts.isLoading);
+  const dispatch = useDispatch();
+  const isLoading = useSelector((state) => state.contacts.loading);
   const error = useSelector((state) => state.contacts.error);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
 
   return (
     <div className="contacts-page">
